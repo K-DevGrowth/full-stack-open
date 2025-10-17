838193ce58a505073be58f0ca5a1dcfd9daaff6c
Khoi <khoidang01234@gmail.com>
2025-10-17 10:26:56 +0700

    feat: add controllers/blogs.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,24 +1,10 @@
 const express = require("express");
-const Blog = require("./models/blog");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
 const app = express();
 
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
