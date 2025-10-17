a8ffd67501db1e19fd998b9cb950ac3df7b3bda3
Khoi <khoidang01234@gmail.com>
2025-10-17 10:34:04 +0700

    refactor to adhere Nodejs best pratices (index.js, app.js, blog.js)

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,10 +1,6 @@
-const express = require("express");
+const app = require("./app");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-const blogsRouter = require("./controllers/blogs");
-const app = express();
-
-app.use("/api/blogs", blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,12 +1,4 @@
 const mongoose = require("mongoose");
-const config = require("../utils/config");
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) =>
-    console.error("Error connecting to MongoDB:", error.message)
-  );
 
 const BlogSchema = new mongoose.Schema({
   title: String,
