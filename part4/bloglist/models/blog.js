cab92e53b1880887b1d7ada80cd8c7ffdb900b10
Khoi <khoidang01234@gmail.com>
2025-10-17 10:01:22 +0700

    init blog list exercise 4-1

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) =>
+    console.error("Error connecting to MongoDB:", error.message)
+  );
+
+const BlogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+BlogSchema.set("toJSON", {
+  transform: function (document, returnedObject) {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Blog", BlogSchema);
