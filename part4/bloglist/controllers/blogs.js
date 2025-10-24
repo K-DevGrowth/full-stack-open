793b2bcdc0c03ba432fca8a19796cf59509c55b3
Khoi <khoidang01234@gmail.com>
2025-10-24 09:34:02 +0700

    Add api test, test_helper, using async-await

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,17 +1,21 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (req, res) => {
+  const blogs = await Blog.find({});
+  res.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  res.json(blog);
+});
+
+blogsRouter.post("/", (req, res) => {
+  const blog = new Blog(req.body);
 
   blog.save().then((result) => {
-    response.status(201).json(result);
+    res.status(201).json(result);
   });
 });
 
