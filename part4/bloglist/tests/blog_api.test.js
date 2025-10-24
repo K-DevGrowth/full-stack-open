e0e19111d7f738cdd9121d5eca669bb8b21771f0
Khoi <khoidang01234@gmail.com>
2025-10-25 05:02:51 +0700

    feat: Add testing for deleted and updated blog_api refactor: Grouping and organization of tests

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -8,112 +8,120 @@ const Blog = require("../models/blog");
 
 const api = supertest(app);
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-  await Blog.insertMany(helper.initialBlogs);
-});
+describe("When there is initially some blogs saved", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
+  });
 
-test("blog are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-test("all blogs are returned", async () => {
-  const blogsAtEnd = await helper.blogsInDB();
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
-});
+  test("all blogs are returned", async () => {
+    const blogsAtEnd = await helper.blogsInDB();
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
 
-test("unique identifier property of blog posts is named id", async () => {
-  const blogsAtEnd = await helper.blogsInDB();
-  const allHaveId = blogsAtEnd.every((blog) => blog.id !== undefined);
-  const nonHave_Id = blogsAtEnd.every((blog) => blog._id === undefined);
+  describe("addition of a blog", () => {
+    test("succeeds with a valid data", async () => {
+      const newBlog = {
+        title: "testing",
+        author: "Khoi",
+        url: "https://testing",
+        likes: 10,
+      };
 
-  assert.strictEqual(allHaveId, true);
-  assert.strictEqual(nonHave_Id, true);
-});
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-test("a sepecific blog is post", async () => {
-  const newBlog = {
-    title: "testing",
-    author: "Khoi",
-    url: "https://testing",
-    likes: 10,
-  };
+      const blogsAtEnd = await helper.blogsInDB();
+      const contents = blogsAtEnd.map((blog) => blog.title);
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+      assert(contents.includes("testing"));
+    });
 
-  const blogsAtEnd = await helper.blogsInDB();
-  const contents = blogsAtEnd.map((blog) => blog.title);
+    test("unique identifier property of blog posts is named id", async () => {
+      const blogsAtEnd = await helper.blogsInDB();
+      const allHaveId = blogsAtEnd.every((blog) => blog.id !== undefined);
+      const nonHave_Id = blogsAtEnd.every((blog) => blog._id === undefined);
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
-  assert(contents.includes("testing"));
-});
+      assert.strictEqual(allHaveId, true);
+      assert.strictEqual(nonHave_Id, true);
+    });
 
-test("the likes property of blog is missing from the request data", async () => {
-  const newBlog = {
-    title: "testing",
-    author: "Khoi",
-    url: "https://testing",
-  };
+    test("succeeds with the likes property is missing", async () => {
+      const newBlog = {
+        title: "testing",
+        author: "Khoi",
+        url: "https://testing",
+      };
 
-  const response = await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+      const response = await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-  assert.strictEqual(response.body.likes, 0);
-});
+      assert.strictEqual(response.body.likes, 0);
+    });
 
-test("the title or url properties are missing from the request data", async () => {
-  const newBlog = {
-    author: "Khoi",
-    likes: 100,
-  };
+    test("fails with status code 400 if title or author is missing", async () => {
+      const newBlog = {
+        author: "Khoi",
+        likes: 100,
+      };
 
-  const blogsAtStart = await helper.blogsInDB();
+      const blogsAtStart = await helper.blogsInDB();
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
+      await api.post("/api/blogs").send(newBlog).expect(400);
 
-  const blogsAtEnd = await helper.blogsInDB();
+      const blogsAtEnd = await helper.blogsInDB();
 
-  assert.strictEqual(blogsAtEnd.length, blogsAtStart.length);
-});
+      assert.strictEqual(blogsAtEnd.length, blogsAtStart.length);
+    });
+  });
 
-test("deleting a blog with valid id", async () => {
-  const blogsAtStart = await helper.blogsInDB();
-  const deletedBlog = blogsAtStart[0];
+  describe("deletion of a blog", () => {
+    test("succeeds with status code 204 if id is valid", async () => {
+      const blogsAtStart = await helper.blogsInDB();
+      const deletedBlog = blogsAtStart[0];
 
-  await api.delete(`/api/blogs/${deletedBlog.id}`).expect(204);
+      await api.delete(`/api/blogs/${deletedBlog.id}`).expect(204);
 
-  const blogsAtEnd = await helper.blogsInDB();
+      const blogsAtEnd = await helper.blogsInDB();
 
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
-});
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+    });
+  });
 
-test("update a blog succeeds with a valid id", async () => {
-  const newBlog = {
-    title: "Willchangelater",
-    author: "A",
-    url: "https://nothing.com",
-    likes: 1,
-  };
-  const blogsAtStart = await helper.blogsInDB();
-  const updatedBlog = blogsAtStart[0];
+  describe("updation of a blog", () => {
+    test("succeeds with valid id", async () => {
+      const newBlog = {
+        title: "Willchangelater",
+        author: "A",
+        url: "https://nothing.com",
+        likes: 1,
+      };
+      const blogsAtStart = await helper.blogsInDB();
+      const updatedBlog = blogsAtStart[0];
 
-  await api.put(`/api/blogs/${updatedBlog.id}`).send(newBlog).expect(200);
+      await api.put(`/api/blogs/${updatedBlog.id}`).send(newBlog).expect(200);
 
-  const blogsAtEnd = await helper.blogsInDB();
-  const contents = blogsAtEnd.map((blog) => blog.title);
+      const blogsAtEnd = await helper.blogsInDB();
+      const contents = blogsAtEnd.map((blog) => blog.title);
 
-  assert.strictEqual(blogsAtStart.length, helper.initialBlogs.length);
-  assert(contents.includes("Willchangelater"));
+      assert.strictEqual(blogsAtStart.length, helper.initialBlogs.length);
+      assert(contents.includes("Willchangelater"));
+    });
+  });
 });
 
 after(async () => {
