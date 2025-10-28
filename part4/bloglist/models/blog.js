015a8051fff270992746e46829bc60fd429e8fdd
Khoi <khoidang01234@gmail.com>
2025-10-29 06:34:48 +0700

    feat: create user routers and user models

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -8,6 +8,10 @@ const BlogSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 BlogSchema.set("toJSON", {
