1a2bd6bd2a326528e24258cba5102213a3ab3c2d
Khoi <khoidang01234@gmail.com>
2026-02-15 09:49:35 +0700

    feat: add user management

---
+++ part13/backendBlog/models/user.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../utils/db");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: "user",
+  },
+);
+
+module.exports = User;
